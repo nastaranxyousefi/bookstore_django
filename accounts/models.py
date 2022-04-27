4e9989a1a2b35f7c2657d757d2ea6f189b6d1793
nastaranxyousefi <nastaranxyousefi@gmail.com>
2022-04-27 18:48:54 +0430

    implementing password reset and change

--- accounts/models.py
+++ accounts/models.py
@@ -4,3 +4,7 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
     age = models.PositiveIntegerField(blank=True, null=True)
 
+
+
+
+
--- config/settings.py
+++ config/settings.py
@@ -139,3 +139,6 @@ LOGOUT_REDIRECT_URL = 'home'
 
 #crispy forms settings
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+#email config
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
