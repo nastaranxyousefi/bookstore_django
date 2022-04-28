dc4f5996dcd0a1dd5cfff24a520dc76168357ba1
nastaranxyousefi <nastaranxyousefi@gmail.com>
2022-04-28 12:01:28 +0430

    add BookListView

--- books/models.py
+++ books/models.py
@@ -8,4 +8,4 @@ class Book(models.Model):
     price = models.DecimalField(max_digits=5, decimal_places=2)
 
     def __str__(self):
-        return self.title
+        return f'{self.title} : {self.author}'
